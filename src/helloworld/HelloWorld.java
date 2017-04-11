9811d4cea3a9d5ef16410767b1672996b4195e0c
sergi <sergi@SERGIO-ALIENWARE>
2017-04-11 18:27:21 -0400

    learding how to use git and github.com The code merely print a single line so the console

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package helloworld;
+
+/**
+ *
+ * @author sergi
+ */
+public class HelloWorld {
+
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to JavaPrograming.");
+    }
+    
+}
